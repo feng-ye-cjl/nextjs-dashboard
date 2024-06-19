d76c383e4cd02c4a26234aecf27814125d3a4952
Reaves <aur-022@hotmail.com>
2024-06-19 10:52:04 +0800

    (overview) page change 3

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,43 +1,41 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import {ArrowRightIcon} from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
-import {lusitana} from "@/app/ui/fonts";
-import Image from "next/image";
+import {Card} from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import {lusitana} from '@/app/ui/fonts';
+import {fetchCardData} from "@/app/lib/data";
+import {Suspense} from "react";
+import {RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton} from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
-export default function Page() {
+export default async function Page() {
+    // 获取税收数据
+    // const revenue = await fetchRevenue();
+    // 最新的发票信息
+    // const latestInvoices = await fetchLatestInvoices();
+
+    // const {
+    //     numberOfCustomers,
+    //     numberOfInvoices,
+    //     totalPaidInvoices,
+    //     totalPendingInvoices,
+    // } = await fetchCardData()
     return (
-        <main className="flex min-h-screen flex-col p-6">
-            {/*<div className={styles.shape}/>*/}
-            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-                <AcmeLogo/>
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                仪表板
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper/>
+                </Suspense>
             </div>
-            <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-                <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-                    {/*<div*/}
-                    {/*    className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"*/}
-                    {/*/>*/}
-                    <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-                        <strong>Welcome to Acme.</strong> This is the example for the{' '}
-                        <a href="https://nextjs.org/learn/" className="text-blue-500">
-                            Next.js Learn Course
-                        </a>
-                        , brought to you by Vercel.
-                    </p>
-                    <Link
-                        href="/login"
-                        className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                    >
-                        <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
-                    </Link>
-                </div>
-                <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-                    {/* Add Hero Images Here */}
-                    <Image src='/hero-desktop.png' width={1000} height={760} className='hidden md:block'
-                           alt="Screenshots of the dashboard project showing desktop version"/>
-                    <Image src='/hero-mobile.png' width={560} height={620} className='hidden md:hidden'
-                           alt="Screenshots of the dashboard project showing mobile version"/>
-                </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton/>}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
